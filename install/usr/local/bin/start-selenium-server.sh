97b74d21a41d30889db8831d869ca0d79d3ba0d7
Ivan <iqsk81ad@gmail.com>
2017-07-15 17:56:15 +0300

    Update start-selenium-server.sh

--- install/usr/local/bin/start-selenium-server.sh
+++ install/usr/local/bin/start-selenium-server.sh
@@ -4,5 +4,4 @@ export DISPLAY=:99
 /etc/init.d/xvfb start
 echo "Starting Google Chrome ..."
 google-chrome --remote-debugging-port=9222 &
-webdriver-manager start
-# java -Dwebdriver.chrome.driver=/srv/chromedriver -jar /srv/selenium-server-standalone-3.4.0.jar
+java -Dwebdriver.chrome.driver=/srv/chromedriver -jar /srv/selenium-server-standalone-3.4.0.jar
